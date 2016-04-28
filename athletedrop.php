eb261ed3c56fc29a5f425c46dc4b5c309bd08332
Michael Hummer <michaelhummer1@gmail.com>
2016-04-28 10:31:48 -0500

    Added files via upload

---
+++ athletedrop.php
@@ -0,0 +1,30 @@
+<?php
+mysql_connect('localhost', 'root', 'csAgan22');
+mysql_select_db('db_project');
+
+$sql  = "SELECT DISTINCT Athletes FROM bjj_analysis";
+$result = mysql_query($sql);
+if(!result)
+{
+	echo "<p>not queried</p>";
+}
+//echo "<p>queried</p>";
+
+echo "<html>";
+echo "<head><link rel='stylesheet' type='text/css' href='main.css'></head>";
+echo "<body bgcolor='#B0E0E6'>";
+echo "<p><a href='index.php'>Home</a></p>";
+echo "<p>Select athlete from dropdown menu and click submit</p>";
+echo "<form method='POST' action='athletescript.php'>";
+echo "<select name='Athlete'>";
+while ($row = mysql_fetch_array($result))
+{
+	echo "<option value='" . $row['Athletes'] . "'>" . $row['Athletes'] . "</option>";
+}
+echo "</select>";
+echo "<input type='submit' value='Submit'>";
+echo "</form>";
+			
+echo "</body>";
+echo "</html>";
+?>
---
+++ athletescript.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body bgcolor="#B0E0E6">
+<?php
+	echo "<p>
+		<a href='index.php'>Home</a>
+		</p>";
+	$db_host = 'localhost';
+	$db_user = 'mhummer';
+	$db_pwd = 'csAgan22';
+
+	$database = 'db_project';
+	if (!mysql_connect($db_host, $db_user, $db_pwd))
+    die("Can't connect to database");
+
+	if (!mysql_select_db($database))
+    die("Can't select database");
+	
+	$athlete = mysql_real_escape_string($_POST['Athlete']);
+	$sql = "SELECT * FROM bjj_analysis WHERE Athletes='{$athlete}'";
+	$result = mysql_query($sql);
+	if(!$result)
+	{
+		die("couldn't find athletes");
+	}
+	$fields_num = mysql_num_fields($result);
+	
+	echo "<h1>{$athlete}</h1>";
+	echo "<table border='1'><tr>";
+	// printing table headers
+	for($i=0; $i<$fields_num; $i++)
+	{
+		$field = mysql_fetch_field($result);
+		echo "<td>{$field->name}</td>";
+	}
+	echo "</tr>\n";
+	// printing table rows
+	while($row = mysql_fetch_row($result))
+	{
+		echo "<tr>";
+
+		// $row is array... foreach( .. ) puts every element
+		// of $row to $cell variable
+		foreach($row as $cell)
+			echo "<td>$cell</td>";
+
+		echo "</tr>\n";
+	}
+mysql_free_result($result);
+?>
+</body>
+</html>
